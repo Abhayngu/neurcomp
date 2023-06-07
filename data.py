5f9201f457b5df3418f1bf3d0a8c48cb174e0326
Abhayngu <masterdwivedi@gmail.com>
2023-06-07 11:17:37 +0530

    comments

--- data.py
+++ data.py
@@ -9,27 +9,54 @@ from torch.utils.data.dataset import Dataset
 
 class VolumeDataset(Dataset):
     def __init__(self,volume,oversample=16):
+        # Resolution of the data [150, 150, 150]
         self.vol_res = volume.shape
+
+        # Number of voxels 150*150*150
         self.n_voxels = th.prod(th.tensor(self.vol_res,dtype=th.int)).item()
+
+        # Resolution in float [150., 150., 150.]
         self.vol_res_float = th.tensor([self.vol_res[0],self.vol_res[1],self.vol_res[2]],dtype=th.float)
+
+        # Minimum value in the volume
         self.min_volume = th.tensor([th.min(volume)],dtype=volume.dtype)
+
+        # Maximum value in the volume
         self.max_volume = th.tensor([th.max(volume)],dtype=volume.dtype)
 
+        # minimum index which can be accessed across all the dimension
         self.min_bb = th.tensor([0.0,0.0,0.0],dtype=th.float)
+
+        # maximum index which can be accessed across all the dimension
         self.max_bb = th.tensor([float(volume.size()[0]-1),float(volume.size()[1]-1),float(volume.size()[2]-1)],dtype=th.float)
+
+        # Difference between max and min indices which can be accessed across all the dimension
         self.diag = self.max_bb-self.min_bb
+
+        # very small value
         self.pos_eps = 1e-8
+
+        # Multiplying by value of almost 1 in diag_eps
+        # Remains same for not very long precision
         self.diag_eps = self.diag*(1.0-2.0*self.pos_eps)
 
+        # Dimension having maximum length
         self.max_dim = th.max(self.diag)
+
+        # scaling every dimension such that everything comes in [0, 1]
         self.scales = self.diag/self.max_dim
         #self.scales = th.ones(3)
 
+        # Tile sampling
         self.lattice = self.tile_sampling(self.min_bb,self.max_bb,res=self.vol_res,normalize=False)
+
+        # Linear Tile sampling
         self.full_tiling = self.tile_sampling(self.min_bb,self.max_bb,res=self.vol_res,normalize=False).view(-1,3)
 
+        # Total number of voxels from the linear tiling
         self.actual_voxels = self.full_tiling.shape[0]
 
+        # Setting oversample
         self.oversample = oversample
     #
 
